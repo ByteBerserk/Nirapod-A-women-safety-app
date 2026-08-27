ea5a3b75badf24d03d9eb60d9446449d90129e3b
promitdebnath <promit.debnath@g.bracu.ac.bd>
2026-08-27 12:09:33 +0600

    Delete backend directory

--- backend/src/routes/safetyResourceRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { listSafetyResources, listSafetyResourcesByCategory } from "../controllers/safetyResourceController.js";
-
-const router = Router();
-
-router.get("/", listSafetyResources);
-router.get("/category/:category", listSafetyResourcesByCategory);
-
-export default router;
